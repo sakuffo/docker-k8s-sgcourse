6ae81c5f0076bcd8b752f16acac6795a44252445
Stephen Akuffo <sa.akuffo@gmail.com>
2021-12-31 08:12:29 -0500

    complex docker files

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There!!!!!
+          Welcome to REACT! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React on the Feature Branch
         </a>
       </header>
     </div>
